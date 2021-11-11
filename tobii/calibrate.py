203b35bc39941da02b43042655d8ad2e377d2e4d
TSUTAYA44 <tatuya7974@gmail.com>
2021-11-11 23:33:55 +0900

    few changed test_calibrate

--- tobii/calibrate.py
+++ tobii/calibrate.py
@@ -6,15 +6,20 @@ class Calibrate:
     def __init__(self, ipv4_address):
         self.ipv4_address = ipv4_address
         self.default = "http://" + ipv4_address + "/rest/"
+        self.time = None
+        self.x = None
+        self.y = None
 
-    def test_calibrate(self, n):
+    def test_calibrate(self):
         """
         n個のcalibrationデータの取得 ※確認 positionデータを返してくれる
         :return [time, x, y]
         """
         requests.post(self.default + "calibrate!emit-markers", data='[]')
-        for i in range(n):
-            return requests.post(self.default + "calibrate:marker", data="").json()
+        _status = requests.post(self.default + "calibrate:marker", data="").json()
+        self.time = _status[0]
+        self.x = _status[1]
+        self.y = _status[2]
 
     def calibrate(self):
         """
